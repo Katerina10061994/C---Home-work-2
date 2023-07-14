ec2ec06a29149b6c7d0b6b445d922e822afd09b0
Katerina <katerina.lapitskaia@gmail.com>
2023-07-14 15:09:06 +0700

    hw task 15

--- task15/Program.cs
+++ task15/Program.cs
@@ -6,13 +6,15 @@
 
 Console.WriteLine("Введите число дня недели");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n >=1 && n <=5);
+if (n >= 1 && n <= 5)
 {
-Console.WriteLine($"будний день");
-}
-    
-if (n >=6 && n <=7);
+    Console.WriteLine($"будний день");
+} 
+else if (n >= 6 && n <= 7)
 {
     Console.WriteLine($"выходной день");
 }
-   
+else if (n >=8)
+{
+    Console.WriteLine($"это не день недели");
+}
